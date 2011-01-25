4d322130dbcf2316dbfd2ee4382374b10b324524
kh <kh@teapot.(none)>
2011-01-25 21:58:25 +0100

    Add Sierpinski code

---
+++ triangle.py
@@ -0,0 +1,13 @@
+#
+# Code from:
+# http://rosettacode.org/wiki/Sierpinski_triangle#Python
+#
+
+def sierpinski(n):
+    d = ["*"]
+    for i in xrange(n):
+        sp = " " * (2 ** i)
+        d = [sp+x+sp for x in d] + [x+" "+x for x in d]
+    return d
+ 
+print "\n".join(sierpinski(4))
