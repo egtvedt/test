df0ffa7c4118e4d8eb721138f373edc655c2ae32
Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>
2011-01-27 09:43:53 +0100

    line: raise an exception if the caller tries to make a line shorter than 0
    Signed-off-by: Hans-Christian Egtvedt <hans-christian.egtvedt@atmel.com>

--- line.py
+++ line.py
@@ -2,6 +2,8 @@ def line(n):
 	"""
 	Return a string containing n "*" elements.
 	"""
+	if n < 0:
+		raise Exception("a line can not be shorter than 0")
 	return "*" * n
 
 if __name__ == "__main__":
